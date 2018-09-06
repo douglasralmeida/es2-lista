03b89014ac2f88e9db7e24455a70573076ae4359
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2018-09-06 01:43:01 -0300

    Conclusão do código

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import pandas as pd
 
 app = Flask(__name__)
+data = pd.read_csv("data.csv").values
+
+def melhorfilme(ano):
+  return data[ano-1929][1]
 
 @app.route('/')
 def home():
@@ -10,16 +15,16 @@ def home():
 def semano():
   return "Erro na URL. Você deve passar um ano no parâmetro."
 
-@app.route("/oscar/<ano>")
-def melhorfilme(ano):
-  if not ano.isdigit():
+@app.route("/oscar/<valor>")
+def oscar(valor):
+  if not valor.isdigit():
     return "Erro na URL. O parâmetro passado deve ser um número inteiro positivo."
-  ano = int(ano)
-  if ano < 1929:
+  valor = int(valor)
+  if valor < 1929:
     return "Erro. A primeira premiação do Oscar ocorreu em 1929."
-  if ano > 2018:
+  if valor > 2018:
     return "Erro. O ano informado é maior que o ano atual."
-  return "Passou."
+  return melhorfilme(valor)
 
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)
