ccd758cd6df689e38523aac15b22b554d457051e
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2018-09-06 01:18:32 -0300

    Validação da URL e parâmetros

--- main.py
+++ main.py
@@ -3,8 +3,23 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-  return 'Ola, mundo!'
+def home():
+  return 'Filme vencedor do Oscar por ano. Use https://url/oscar/ano'
+
+@app.route("/oscar/")
+def semano():
+  return "Erro na URL. Você deve passar um ano no parâmetro."
+
+@app.route("/oscar/<ano>")
+def melhorfilme(ano):
+  if not ano.isdigit():
+    return "Erro na URL. O parâmetro passado deve ser um número inteiro positivo."
+  ano = int(ano)
+  if ano < 1929:
+    return "Erro. A primeira premiação do Oscar ocorreu em 1929."
+  if ano > 2018:
+    return "Erro. O ano informado é maior que o ano atual."
+  return "Passou."
 
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)
